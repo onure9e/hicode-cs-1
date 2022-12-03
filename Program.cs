e7eccfc6012438fc47b534cf8669360bf75f014a
Onurege <78586675+onure9e@users.noreply.github.com>
2022-12-03 23:59:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("> İsmin: ");
+            string isim= Console.ReadLine();
+            Console.WriteLine("> Soy İsimin: ");
+            string soyisim=Console.ReadLine();
+            Console.WriteLine("Merhaba!"+" "+isim+" "+soyisim);
+            
+        }
+    }
+}
